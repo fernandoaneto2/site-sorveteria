355fe37abfdb228fbb9067002abad6d4553dd947
fernandoaneto2 <fernando.neto02@gmail.com>
2025-03-13 09:50:02 -0300

    ajustando next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,16 @@
 import type { NextConfig } from "next";
+const path = require("path");
+
+module.exports = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // Se estiver usando Next.js 13+ com app router
+  },
+  webpack: (config) => {
+    config.resolve.modules.push(path.resolve(__dirname, "src"));
+    return config;
+  },
+};
 
 const nextConfig: NextConfig = {
   /* config options here */
